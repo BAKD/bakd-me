add5e0ef0d823ecf53e6d479179dec706517a039
Tom G <tom@bakd.io>
2018-11-10 15:25:33 -0500

    Cleanup vue route catchall, add exceptions for manage backup routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
-// Catchall for vue single page app requests.
-//  
-// Ignore requests to /manage or nova-api, as we need those to run specific middleware
-// in order for Nova to pick up the session, etc.
-Route::get('{segments}', 'SiteController@handleVueRequests')->where('segments', '^((?!manage|nova-api).)*$')->middleware('spa');
+// Route Whitelist
+// Catchall ignores requests to any of the endpoints noted below.
+$except = [
+	'manage',
+	'nova-api',
+	'backup-statuses',
+	'backups',
+	'_debugbar'
+];
+
+// Catchall for any Vue related requests.
+Route::get('{segments}', 'SiteController@handleVueRequests')
+	->where('segments', '^((?!' . implode('|', $except) . ').)*$')
+	->middleware('spa');
