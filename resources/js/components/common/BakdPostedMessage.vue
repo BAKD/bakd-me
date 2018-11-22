921da5319aa87adaac72e2b4c5dd68cc6f0af763
Tom Grenier <tom@bakd.io>
2018-11-22 12:32:27 -0500

    Posted message component tweaks

--- resources/js/components/common/BakdPostedMessage.vue
+++ resources/js/components/common/BakdPostedMessage.vue
@@ -1,12 +1,13 @@
 <template>
     <div class="box posted-message">
       <article class="media">
-        
 
         <!-- AVATAR/INFO -->
         <figure class="media-left has-text-centered" style="width: 64px;">
           <p class="image">
-            <img :src="user.avatar_url || user.photo_url" class="is-centered-margin image" style="width: 100%; height: auto; max-width: 64px;" />
+            <router-link :to="{ name: 'members.show.public', params: { id: post.user.id } }">
+              <img :src="user.avatar_url || user.photo_url" class="is-centered-margin image" style="width: 100%; height: auto; max-width: 64px;" />
+            </router-link>
           </p>
           <p class="subtitle is-size-7" style="padding-top: 6px;">
             {{ post.user.type | capitalize }}
@@ -14,61 +15,60 @@
           <p class="trust-widget is-size-7 has-text-centered">
             <span class="is-bold">Trust:</span> <span class="is-green is-bold">+10,200</span>
           </p>
-          <p>
 
-<!-- 
-                <ul class="is-grouped field is-start is-pulled-bottom has-text-centered">
-
-                  <li>
-                    <b-tooltip label="Like" type="is-dark">
-                      <a href="#" class="has-text-dark">
-                        <div class="icon">
-                          <fa icon="thumbs-up" />
-                        </div>
-                      </a>
-                    </b-tooltip>
-                  </li>
-
-                  <li>
-                    <b-tooltip label="Dislike" type="is-dark">
-                      <a href="#" class="has-text-dark">
-                        <div class="icon">
-                          <fa icon="thumbs-down" />
-                        </div>
-                      </a>
-                    </b-tooltip>
-                  </li>
-
-                </ul> -->
-            <!-- <img src="https://via.placeholder.com/50" /> -->
-          </p>
+          <!-- <p>
+            <ul class="has-text-centered">
+              <li class="inline-block">
+                <b-tooltip label="Give Trust" type="is-dark">
+                  <a href="#" class="has-text-dark">
+                    <div class="icon">
+                      <i class="la la-chevron-up" />
+                    </div>
+                  </a>
+                </b-tooltip>
+              </li>
+              <li class="inline-block">
+                <b-tooltip label="Lose Trust" type="is-dark">
+                  <a href="#" class="has-text-dark">
+                    <div class="icon">
+                      <i class="la la-chevron-down" />
+                    </div>
+                  </a>
+                </b-tooltip>
+              </li>
+            </ul>
+          </p> -->
         </figure>
-
 
         <!-- MAIN POST CONTENT -->
         <div class="media-content">
           <div class="level">
-
             <div class="level-left">
-              <span class="is-bold">{{ post.user.name }}</span>&nbsp;&nbsp;<span class="is-size-7">{{ post.created_at | fromNow }}...</span>
+              <span class="is-bold">
+              <router-link :to="{ name: 'members.show.public', params: { id: post.user.id } }">
+                {{ post.user.name }}
+              </router-link>
+            </span>
+            &nbsp;&nbsp;
+            <span class="is-size-7" :title="post.created_at">
+                <i class="la la-clock-o"></i> {{ post.created_at | fromNow }}...
+            </span>
             </div>
-
             <div class="level-right is-size-7 has-text-right">
               <p class="is-size-7" style="max-width: 30px;">
                 <b-tooltip label="View Conversation"type="is-dark" position="is-left">
-                  <fa icon="chevron-down" />
+                  <a href="#" class="has-text-dark" @click.prevent="">
+                    <fa icon="chevron-down" />
+                  </a>
                 </b-tooltip>
               </p>
             </div>
-
           </div>
-
 
           <!-- Main Message Section -->
           <div class="field" style="margin-bottom: 0;">
             <p class="control" style="padding-top: 0px;">
               <b-message style="margin-bottom: 20px; padding: 20px;" v-html="post.message"></b-message>
-
               <!-- TODO: Setup Tag System -->
               <ul class="tags field is-grouped">
                 <a href="#">
@@ -94,92 +94,71 @@
               </ul>
             </p>
           </div>
-
-
-          <!-- FOOTER ICONS -->
-          <nav class="level" style="margin-top: 5px;">
-            <div class="level-item has-text-left">
-              <div class="is-size-7">
-
-
-
-
-
-
-              </div>
-            </div>
-
-            <!-- TOP RIGHT -->
-            <div class="media-right">
-
-
-              <ul class="is-grouped field is-end is-clipped">
-
-
-<!-- 
-                  <li style="margin-right: 10px;">
-                    <b-tooltip label="Like" type="is-dark">
-                      <a href="#" class="has-text-dark">
-                        <div class="icon">
-                          <fa icon="heart" class="has-text-red" />
-                        </div>
-                      </a>
-                    </b-tooltip>
-                  </li> -->
-<!-- 
-                  <li style="margin-right: 10px;">
-                    <b-tooltip label="Dislike" type="is-dark">
-                      <a href="#" class="has-text-dark">
-                        <div class="icon">
-                          <fa icon="thumbs-down" />
-                        </div>
-                      </a>
-                    </b-tooltip>
-                  </li> -->
-
-
-                <li style="margin-right: 10px;">
-                  <b-tooltip label="Comment" type="is-dark">
-                    <a href="#" class="has-text-dark">
-                      <div class="icon">
-                        <!-- <fa icon="comment" /> -->
-                        <i class="la la-comment is-size-5" />
-                      </div>
-                    </a>
-                  </b-tooltip>
-                </li>
-                <li style="margin-right: 10px;">
-                  <b-tooltip label="Reshare" type="is-dark">
-                    <a href="#" class="has-text-dark">
-                      <div class="icon">
-                        <!-- <fa icon="share" /> -->
-                        <i class="la la-share is-size-5" />
-                      </div>
-                    </a>
-                  </b-tooltip>
-                </li>
-                <li>
-
-                  <b-tooltip label="More" type="is-dark">
-                    <b-dropdown class="dropdown-trigger-icon" position="is-bottom-left">
-                      <div class="icon is-v-centered" slot="trigger">
-                        <!-- <fa icon="ellipsis-v"/> -->
-                        <i class="la la-ellipsis-v is-size-5" />
-                      </div>
-                      <b-dropdown-item><i class="la la-ban"/> Mute Member</b-dropdown-item>
-                      <b-dropdown-item><i class="la la-bell"/> Report Message</b-dropdown-item>
-                    </b-dropdown>
-                  </b-tooltip>
-
-                </li>
-
-              </ul>
-            </div>
-      
-          </nav>
         </div>
-
       </article>
+
+        <!-- FOOTER ICONS -->
+        <nav class="level" style="margin-top: 5px;">
+          <div class="level-item has-text-left">
+            <div class="is-size-7">
+              <!-- ... -->
+            </div>
+          </div>
+
+          <!-- TOP RIGHT -->
+          <div class="media-right">
+            <ul class="is-grouped field is-end">
+              <li style="margin-right: 10px;">
+                <b-tooltip label="Like" type="is-dark" position="is-bottom">
+                  <a href="#" class="has-text-dark" @click.prevent="">
+                    <div class="icon">
+                      <i class="la la-thumbs-up is-size-5" />
+                    </div>
+                  </a>
+                </b-tooltip>
+              </li>
+              <li style="margin-right: 10px;">
+                <b-tooltip label="Dislike" type="is-dark" position="is-bottom">
+                  <a href="#" class="has-text-dark" @click.prevent="">
+                    <div class="icon">
+                      <i class="la la-thumbs-down is-size-5" />
+                    </div>
+                  </a>
+                </b-tooltip>
+              </li>
+              <li style="margin-right: 10px;">
+                <b-tooltip label="Comment" type="is-dark" position="is-bottom">
+                  <a href="#" class="has-text-dark" @click.prevent="">
+                    <div class="icon">
+                      <i class="la la-comment is-size-5" />
+                    </div>
+                  </a>
+                </b-tooltip>
+              </li>
+              <!-- <li style="margin-right: 10px;">
+                <b-tooltip label="Reshare" type="is-dark" position="is-bottom">
+                  <a href="#" class="has-text-dark" @click.prevent="">
+                    <div class="icon">
+                      <i class="la la-share is-size-5" />
+                    </div>
+                  </a>
+                </b-tooltip>
+              </li> -->
+              <li>
+                <b-tooltip label="More" type="is-dark" position="is-bottom">
+                  <b-dropdown class="dropdown-trigger-icon" position="is-bottom-left">
+                    <div class="icon is-v-centered" slot="trigger">
+                      <i class="la la-ellipsis-v is-size-5" />
+                    </div>
+                    <b-dropdown-item><i class="la la-share"/> Reshare</b-dropdown-item>
+                    <b-dropdown-item><i class="la la-ban"/> Mute Member</b-dropdown-item>
+                    <b-dropdown-item><i class="la la-bell"/> Report Message</b-dropdown-item>
+                  </b-dropdown>
+                </b-tooltip>
+              </li>
+            </ul>
+          </div>
+        </nav>
     </div>
 </template>
 
@@ -187,7 +166,6 @@
 import { mapGetters } from "vuex"
 
 export default {
-
   props: {
     post: {
       type: Object,
@@ -208,11 +186,18 @@ export default {
   },
 
   mounted() {
+    // If this post belongs to the currently auth'd user, use the vuex store
+    // value for user instead of the ajax response data so when a new avatar
+    // is uploaded, it is reactive
     if (this.authdUser.id == this.$props.post.user.id) {
+      // Vuex user
       this.user = this.authdUser
     } else {
+      // Ajax response data/DB relationship post.user
       this.user = this.$props.post.user
     }
+
+    // Grab the avatar from whichever user is currently being used
     this.avatar = this.user.avatar_url || this.user.photo_url
   }
 
@@ -222,5 +207,8 @@ export default {
 <style lang="scss" scoped>
   .posted-message {
     margin: 0 0 20px 0;
+  }
+  .inline-block {
+    display: inline-block;
   }
 </style>
